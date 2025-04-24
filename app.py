120a087e1ef06e6e7a6f2e597fe5a16fb1bb5489
Bukolaaa <125439105+Bukolaaa@users.noreply.github.com>
2025-04-24 15:34:07 +0100

    Update app.py error

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-<<<<<<< HEAD
-    return "<h1>Hello, Bukola's first Cloud App!....yaaay!</h1><p>This is running on Flask and Azure!</p>"
-=======
     return "<h1>Hello, Bukola's Cloud App!</h1><p>This is running on Flask and this is running live on Azure!</p>"
->>>>>>> 74e54b2 (new commit)
 
 if __name__ == '__main__':
     app.run(debug=True)
